33bfaf4ec7c011298c78b11c9007981f302c2f85
dqget <diaoqiang2006@163.com>
2019-03-20 11:09:13 +0800

    增加枚举类

--- src/main/java/com/lovesickness/o2o/enums/OrderStateEnum.java
+++ src/main/java/com/lovesickness/o2o/enums/OrderStateEnum.java
@@ -1,23 +1,26 @@
 package com.lovesickness.o2o.enums;
 
-public enum ProductStateEnum {
-    OFFLINE(-1, "非法商品"),
+public enum OrderStateEnum {
+    //订单操作
+    OFFLINE(-1, "非法订单"),
     SUCCESS(0, "操作成功"),
     PASS(2, "通过认证"),
     INNER_ERROR(-1001, "操作失败"),
-    EMPTY(-1002, "商品为空");
+    EMPTY(-1002, "订单为空"),
+    EMPTY_MAP(-1003, "订单项为空");
 
     private int state;
 
     private String stateInfo;
 
-    private ProductStateEnum(int state, String stateInfo) {
+    private OrderStateEnum(int state, String stateInfo) {
         this.state = state;
         this.stateInfo = stateInfo;
     }
 
-    public static ProductStateEnum stateOf(int index) {
-        for (ProductStateEnum state : values()) {
+
+    public static OrderStateEnum stateOf(int index) {
+        for (OrderStateEnum state : values()) {
             if (state.getState() == index) {
                 return state;
             }
@@ -32,5 +35,4 @@ public enum ProductStateEnum {
     public String getStateInfo() {
         return stateInfo;
     }
-
 }
